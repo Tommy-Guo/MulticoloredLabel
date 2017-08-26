c32b428b053b16d449fab25554ecbc8bb91ade25
VmX5 <SockMonkeyboy@hotmail.com>
2017-08-26 18:17:22 -0400

    Initial Commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.MultiLabel1 = New dotNet_MultiLabel.MultiLabel()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.White
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(1, 1)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(40, 46)
+        Me.Panel1.TabIndex = 4
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(86, 72)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(88, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(219, 72)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(78, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Black
+        Me.Panel2.Controls.Add(Me.MultiLabel1)
+        Me.Panel2.Location = New System.Drawing.Point(12, 12)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Padding = New System.Windows.Forms.Padding(1)
+        Me.Panel2.Size = New System.Drawing.Size(285, 54)
+        Me.Panel2.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(141, 98)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.IntegralHeight = False
+        Me.ListBox1.Location = New System.Drawing.Point(303, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(291, 108)
+        Me.ListBox1.TabIndex = 7
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(222, 98)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "Remove"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(60, 98)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Update"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.Black
+        Me.Panel3.Controls.Add(Me.Panel1)
+        Me.Panel3.Location = New System.Drawing.Point(12, 72)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Padding = New System.Windows.Forms.Padding(1)
+        Me.Panel3.Size = New System.Drawing.Size(42, 48)
+        Me.Panel3.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(54, 77)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Start:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(175, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Length:"
+        '
+        'MultiLabel1
+        '
+        Me.MultiLabel1.BackColor = System.Drawing.Color.White
+        Me.MultiLabel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.MultiLabel1.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MultiLabel1.Location = New System.Drawing.Point(1, 1)
+        Me.MultiLabel1.Name = "MultiLabel1"
+        Me.MultiLabel1.Size = New System.Drawing.Size(283, 52)
+        Me.MultiLabel1.TabIndex = 0
+        Me.MultiLabel1.Text = "RED GREEN BLUE"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(605, 129)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Name = "Form1"
+        Me.Text = "MultiLabel"
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MultiLabel1 As dotNet_MultiLabel.MultiLabel
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,86 @@
+﻿Public Class Form1 
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Add(String.Format("StartIndex: {0} |  Length: {1} |  Color: {2}", TextBox1.Text, TextBox2.Text, Panel1.BackColor.R & ", " & Panel1.BackColor.G & ", " & Panel1.BackColor.B))
+        Button3.PerformClick()
+    End Sub
+
+    Private Sub Panel1_Click(sender As Object, e As EventArgs) Handles Panel1.Click
+        Dim nColorDialog As New ColorDialog
+        If nColorDialog.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Panel1.BackColor = nColorDialog.Color
+        End If
+    End Sub 
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MultiLabel1._charSelection.Clear()
+
+        For Each charInfo As String In ListBox1.Items
+            Dim charInfoString() As String = charInfo.Replace("StartIndex:", "").Replace("Length:", "").Replace("Color:", "").Replace(" ", "").Split("|"c)
+            Dim nSelection As New CharSelection
+            Dim colorSplit() As String = charInfoString(2).Split(","c)
+            nSelection.Color = Color.FromArgb(colorSplit(0), colorSplit(1), colorSplit(2))
+            nSelection.StartIndex = Convert.ToInt32(charInfoString(0))
+            nSelection.Length = Convert.ToInt32(charInfoString(1))
+            MultiLabel1._charSelection.Add(nSelection)
+        Next
+
+        MultiLabel1.Invalidate()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            ListBox1.Items.Remove(ListBox1.SelectedItem)
+        Catch
+        End Try
+    End Sub
+End Class
+
+Public Class MultiLabel
+    Inherits Control
+
+    Public _charSelection As New List(Of CharSelection) 
+
+    Protected Overrides Sub OnPaint(e As PaintEventArgs)
+        MyBase.OnPaint(e)
+        Dim g As Graphics = e.Graphics
+        g.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+        g.TextRenderingHint = Drawing.Text.TextRenderingHint.AntiAlias
+        g.Clear(BackColor)
+
+        Dim txtChars As Char() = Text.ToArray
+
+        If _charSelection.Count = 0 Then
+
+            g.DrawString(Text, Font, Brushes.Black, New Rectangle(3, 3, Width - 3, Height - 3))
+
+        Else
+
+            Dim x, y As Integer
+            For i As Integer = 0 To txtChars.Count - 1
+                For c As Integer = 0 To _charSelection.Count - 1
+                    If i + 1 > _charSelection(c).StartIndex And i < _charSelection(c).StartIndex + _charSelection(c).Length Then
+                        g.DrawString(txtChars(i), Font, New SolidBrush(_charSelection(c).Color), x, y)
+                        Exit For
+                    Else
+                        g.DrawString(txtChars(i), Font, Brushes.Black, x, y)
+                    End If
+                Next
+                x += g.MeasureString(txtChars(i), Font).Width
+                If y > Width - 20 Then
+                    x = 0
+                    y += g.MeasureString(txtChars(i), Font).Height + 2
+                End If
+            Next
+
+        End If
+
+    End Sub 
+End Class
+
+Public Class CharSelection
+
+    Public Color As Color = Color.Black
+    Public StartIndex As Integer = 1
+    Public Length As Integer = 1
+End Class
